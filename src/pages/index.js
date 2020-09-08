c5da1b87bcac182be63243db393aafc7afa198f2
chigozieorunta <chigozieorunta@yahoo.com>
2020-09-09 00:26:09 +0100

    Refactored Home component, added Formik component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,12 @@ import React from "react"
 import { Formik } from "formik"
 import * as Yup from "yup"
 
-export default function Home() {
-  return <div>Testing Build</div>
+const Home = () => {
+  return (
+    <>
+      <Formik></Formik>
+    </>
+  )
 }
+
+export default Home
