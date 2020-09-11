2d48f52197fb8ff368b91ec9ca717e0f7313b56e
chigozieorunta <chigozieorunta@yahoo.com>
2020-09-11 11:43:07 +0100

    Abstracted layout container seperately

---
+++ src/components/layout.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { Container, Row, Col } from "react-bootstrap"
+
+const Layout = () => {
+  return (
+    <>
+      <section className="d-flex vh-100 align-items-center">
+        <Container fluid>
+          <Row>
+            <Col sm={{ span: 4, offset: 4 }}>{props.children}</Col>
+          </Row>
+        </Container>
+      </section>
+    </>
+  )
+}
+
+export default Layout
