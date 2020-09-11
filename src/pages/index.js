a047b08a941ee9ec8c2a07cfebf179f616b75a5a
chigozieorunta <chigozieorunta@yahoo.com>
2020-09-11 11:48:34 +0100

    Positioned Formik within Layout component

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,28 +22,30 @@ const TextField = ({ label, ...props }) => {
 const Home = () => {
   return (
     <>
-      <Formik
-        initialValues={{
-          firstName: "",
-          lastName: "",
-          email: "",
-          password: "",
-        }}
-        validationScheme={Yup.object({
-          firstName: Yup.string()
-            .min(2, "Must be at least 2 characters")
-            .required("Required"),
-          lastName: Yup.string()
-            .min(2, "Must be at least 2 characters")
-            .required("Required"),
-          email: Yup.string()
-            .email("Invalid e-mail address")
-            .required("Required"),
-          Password: Yup.string()
-            .min(6, "Must be at least 6 characters")
-            .required("Required"),
-        })}
-      ></Formik>
+      <Layout>
+        <Formik
+          initialValues={{
+            firstName: "",
+            lastName: "",
+            email: "",
+            password: "",
+          }}
+          validationScheme={Yup.object({
+            firstName: Yup.string()
+              .min(2, "Must be at least 2 characters")
+              .required("Required"),
+            lastName: Yup.string()
+              .min(2, "Must be at least 2 characters")
+              .required("Required"),
+            email: Yup.string()
+              .email("Invalid e-mail address")
+              .required("Required"),
+            Password: Yup.string()
+              .min(6, "Must be at least 6 characters")
+              .required("Required"),
+          })}
+        ></Formik>
+      </Layout>
     </>
   )
 }
